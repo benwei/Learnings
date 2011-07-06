c2699167c8d0e3f95286696e41fc92c3c35e7295
Ben Wei <ben.apps@yahoo.com.tw>
2011-07-06 17:22:30 +0800

    module reference

--- nodejsSamples/include-module.js
+++ nodejsSamples/include-module.js
@@ -1,3 +1,4 @@
+// sample from http://nodejs.org/docs/v0.4.9/api/modules.html
 var circle = require('./modules/circle.js');
 console.log( 'The area of a circle of radius 4 is '
            + circle.area(4));
