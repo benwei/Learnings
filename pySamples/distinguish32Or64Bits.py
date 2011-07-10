f69b2acf43c653c1e0d55001d330aed0cf7ca200
Ben Wei <ben.apps@yahoo.com.tw>
2011-07-11 02:19:06 +0800

    add cygwin results

--- pySamples/distinguish32Or64Bits.py
+++ pySamples/distinguish32Or64Bits.py
@@ -10,7 +10,11 @@ print "hello: %u" % sys.maxint
 # 2**30 -1 = 2147483647
 print "log(sys.maxsize, 2) = %d" % (log(sys.maxsize, 2))
 
-# Results on 10.6.7 (SnowLeopard)
+## Results on 10.6.7 (SnowLeopard 64-bit)
 # maxsize: 9223372036854775807
 # hello: 9223372036854775807
 # log(sys.maxsize, 2) = 63
+## Results on cygwin (Windows xp sp3 64-bit)
+# maxsize: 2147483647
+# hello: 2147483647
+# log(sys.maxsize, 2) = 30
