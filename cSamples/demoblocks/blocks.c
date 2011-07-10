708ef59ac2fc2134d0a9f1a4e4e29d71cbe3b76d
Ben Wei <ben.apps@yahoo.com.tw>
2011-07-10 12:29:35 +0800

    add blocks.c

---
+++ cSamples/demoblocks/blocks.c
@@ -0,0 +1,47 @@
+/*
+ *  blocks.c
+ *  demoblocks
+ *
+ *  Created by boslab on 6/30/11.
+ *  Copyright 2011 boslab. All rights reserved.
+ *
+ */
+#include <Block.h>
+#include <stdio.h>
+
+// define block
+typedef int (^IntBlock)();
+
+// In mathematics, an arithmetic progression (AP) or arithmetic sequence is a sequence of
+// numbers such that the difference of any two successive members of the sequence is a constant. For instance, the sequence 3, 5, 7, 9, 11, 13,
+// … is an arithmetic progression with common difference 2.
+
+IntBlock mathAP(int firstTerm, int difference) {
+	__block int  computeTerm = firstTerm;
+	
+	return Block_copy( ^ {
+		computeTerm += difference;
+		return computeTerm;
+	});
+}
+
+int mathAPloop(int n, IntBlock blockAP)
+{
+	int m = n;
+	while (n-- > 0) {
+		printf("a%d: %d\n", m - n, blockAP());
+	}
+	
+	return 0;
+}
+
+int main() {
+	
+	IntBlock blockAP = mathAP(1, 3);
+	
+	
+	mathAPloop(10, blockAP);
+	
+	Block_release(blockAP);
+	return 0;
+}
