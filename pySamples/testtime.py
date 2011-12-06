c27a45ed6d00e1f2e4cd4d4f9bb1af45c996a4a9
ben wei <ben@staros.mobi>
2011-12-06 18:09:11 +0800

    added getSummseryDateStr

--- pySamples/testtime.py
+++ pySamples/testtime.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import time
+import datetime
 
 # gmtime()
 gmt = time.gmtime()
@@ -15,12 +16,28 @@ print t
 # strftime()
 print time.strftime("%Y-%m-%d")
 
-import datetime
-print "date: %s" % datetime.date.fromtimestamp(time.time())
-# datetime timedelta
-b = datetime.datetime(2009, 2, 1, 22)
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+def getSummeryDateStr():
+    d = datetime.date.fromtimestamp(time.time())
+    print "date: %s" % d
+    print "year-month: %s-%s-%s" % (d.year, d.month, d.day)
+    # datetime timedelta
+    d_offset = d + relativedelta( months = +1)
+    b = datetime.datetime(d_offset.year, d_offset.month, 1)
+    e = datetime.timedelta(seconds=1)
+    return (b - e)
+
+
+print "getSummeryDateStr = %s" % getSummeryDateStr()
+
+d = datetime.date.fromtimestamp(time.time())
+six_months = d + relativedelta( months = +6 )
+print six_months
+
+b = datetime.datetime(d.year, d.month, 1)
 a = datetime.timedelta(hours=10)
-print b
 print b - a
 c = datetime.timedelta(seconds=10)
 print b - c
