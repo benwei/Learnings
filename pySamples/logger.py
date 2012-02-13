44e936efe437062de1b5adcc3c7a58b79a904c9a
ben wei <ben@staros.mobi>
2012-02-13 11:24:39 +0800

    add warning test for logger

--- pySamples/logger.py
+++ pySamples/logger.py
@@ -4,4 +4,4 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 logging.getLogger().setLevel(logging.DEBUG)
 logging.debug("test sring")
-
+logging.warning("warning test")
