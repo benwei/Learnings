94bdd456a57a993c530a19a01bbde9281a2aba60
ben wei <ben@staros.mobi>
2012-06-20 16:05:50 +0800

    [performance] bash script to list line of code per file

--- bashSamples/line_of_code_perfile.sh
+++ bashSamples/line_of_code_perfile.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 find . | while read fn ; do
 if [ ! -d "$fn" ] ; then
-	 line=`cat $fn | wc -l | sed -e 's/[ ]*//g'` ;
-	 fn=`echo $fn | sed -e 's/^[./]*//g'`
 	 case $fn in
 	 *.js|*.sh)
+	 line=`cat $fn | wc -l | sed -e 's/[ ]*//g'` ;
+	 fn=`echo $fn | sed -e 's/^[./]*//g'`
 	 echo "$fn: $line" ;
 	 ;;
 	 esac
