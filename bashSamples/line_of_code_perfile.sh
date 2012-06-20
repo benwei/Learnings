e8d91df30d824e1be20d7e4b3df1f7494d0c72b5
ben wei <ben@staros.mobi>
2012-06-20 16:11:12 +0800

    [refactor] verbose option for bash script to list line of code per file

--- bashSamples/line_of_code_perfile.sh
+++ bashSamples/line_of_code_perfile.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
+verbose=0
+
+opt="$1"
+
+if [ "$opt" = "-v" ] ; then
+	verbose=1;
+fi
+
 find . | while read fn ; do
 if [ ! -d "$fn" ] ; then
-	 case $fn in
-	 *.js|*.sh)
-	 line=`cat $fn | wc -l | sed -e 's/[ ]*//g'` ;
-	 fn=`echo $fn | sed -e 's/^[./]*//g'`
-	 echo "$fn: $line" ;
-	 ;;
-	 esac
+	case $fn in
+	*.js|*.sh)
+	line=`cat $fn | wc -l | sed -e 's/[ ]*//g'` ;
+	fn=`echo $fn | sed -e 's/^[./]*//g'`
+	echo "$fn: $line" ;
+	;;
+	*)
+	if [ "$verbose" -gt 0 ]; then
+	echo "skip file $fn"
+	fi
+	;;
+	esac
 fi ;
 done
