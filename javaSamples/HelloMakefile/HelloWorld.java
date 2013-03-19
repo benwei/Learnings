ddd8b61d61bfbb9f1662edc3647b124d4362685b
ben wei <ben@staros.mobi>
2013-03-19 17:17:30 +0800

    [java] sample with Makefile

---
+++ javaSamples/HelloMakefile/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main( String[] args )
+    {
+        int a = 0;
+        int b = 1;
+        a = a > b ? a : b;
+
+        System.out.println("Hello world! " + a + " "+ b);
+    } // end main
+}
