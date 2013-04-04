dd8cf1b8ba3003df2caf80d5d176938fd24c1aa3
ben wei <ben@staros.mobi>
2013-04-05 02:28:15 +0800

    [Inherts] book Good Parts sample

---
+++ nodejsSamples/testInherits.js
@@ -0,0 +1,31 @@
+require('./lib/Method');
+
+// reference the good parts : page 49
+
+Function.method('inherits', function (Parent) {
+    this.prototype = new Parent();
+    return this;
+});
+
+function Book(name, price)
+{
+    this.name = name;
+    this.price = price;
+    this.type = 'book';
+}
+
+Book.method('browse', function (){
+    return "The name of the book is '" + this.name + "' which costs " + this.price + ".";
+});
+
+
+var Bookjs = function (name, price) {
+    this.name = name;
+    this.category = 'Javascript';
+    this.price = price;
+}.inherits(Book).method('get_category', function (){
+    return this.browse() + " Category is " + this.category + ".";
+});
+
+var book = new Bookjs('JS Good Parts', 300);
+console.log(book.get_category());
