2f4e77c31f996e8cb48cc5bec197d82a88535200
ben wei <ben@staros.mobi>
2013-08-21 14:53:54 +0800

    [case] add mutex lock

--- cSamples/threads/threads.c
+++ cSamples/threads/threads.c
@@ -12,12 +12,15 @@ struct targs {
 static unsigned long up_counter = 0;
 
 typedef void *(*thread_routine)(void *);
+static pthread_mutex_t thread_lock = PTHREAD_MUTEX_INITIALIZER;
 
 static void thread_monitor(struct targs *arg)
 {
     do {
         /* without lock the up_counter will be correct */
+        pthread_mutex_lock(&thread_lock);
         up_counter++;
+        pthread_mutex_unlock(&thread_lock);
         printf("thread%d(%08lx) A%d %lu\n", arg->num, (long)arg->thread,  arg->counter, up_counter);
         usleep(500);
     } while (arg->counter++ < arg->max) ;
@@ -28,6 +31,7 @@ int main()
     struct targs threads[2] = {};
     void *value = NULL;
     int i = 0;
+    pthread_mutex_init(&thread_lock, NULL);
     for (i = 0; i < 2; i++) {
         struct targs *ta = &threads[i];
         printf("thread %d created\n", i);
@@ -41,5 +45,6 @@ int main()
         struct targs *ta = &threads[i];
         pthread_join(ta->thread, &value);
     }
+    pthread_mutex_destroy(&thread_lock);
     return 0;
 }
