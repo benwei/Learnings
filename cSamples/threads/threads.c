bcca9617e11f675265d3382ad0a76c8b05aafa4b
ben wei <ben@staros.mobi>
2013-08-21 14:47:12 +0800

    [case] 2 threads without lock

--- cSamples/threads/threads.c
+++ cSamples/threads/threads.c
@@ -6,26 +6,40 @@ struct targs {
     pthread_t thread;
     int num;
     int counter;
+    int max;
 };
+
+static unsigned long up_counter = 0;
 
 typedef void *(*thread_routine)(void *);
 
-void thread_monitor(struct targs *arg)
+static void thread_monitor(struct targs *arg)
 {
     do {
-        printf("thread%d(%08lx) A%d\n", arg->num, (long)arg->thread,  arg->counter);
+        /* without lock the up_counter will be correct */
+        up_counter++;
+        printf("thread%d(%08lx) A%d %lu\n", arg->num, (long)arg->thread,  arg->counter, up_counter);
         usleep(500);
-    } while (arg->counter++ < 10) ;
+    } while (arg->counter++ < arg->max) ;
 }
 
 int main()
 {
-    struct targs ta = {};
+    struct targs threads[2] = {};
     void *value = NULL;
-    printf("thread creation\n");
-    ta.num = 1;
-    ta.counter = 1;
-    pthread_create(&ta.thread, NULL, (thread_routine) thread_monitor, &ta);
-    pthread_join(ta.thread, &value);
+    int i = 0;
+    for (i = 0; i < 2; i++) {
+        struct targs *ta = &threads[i];
+        printf("thread %d created\n", i);
+        ta->num = i;
+        ta->counter = 1;
+        ta->max = 5;
+        pthread_create(&ta->thread, NULL, (thread_routine) thread_monitor, ta);
+    }
+
+    for (i = 0; i < 2; i++) {
+        struct targs *ta = &threads[i];
+        pthread_join(ta->thread, &value);
+    }
     return 0;
 }
