adc6476de71a393c883bbaac8a9a5d932c776fa6
ben wei <ben@staros.mobi>
2013-10-21 10:01:07 +0800

    c-sample: hello with volatile

--- cSamples/gcc-static-inline/hello.c
+++ cSamples/gcc-static-inline/hello.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 
 
-void funca()
+void funca(int a)
 {
+    return a+1;
 }
 
-static void funcb()
+static void funcb(int b)
 {
+    return b+2;
 }
 
-static inline void funcc()
+static inline void funcc(int c)
 {
+    return c+3;
 }
 
 int main()
 {
-	funcb();
-#if 0
+	funcb(1);
+#if 1
 	int a = 1; // 3 useless line will be removed by -O2 optimization
-	int b = 1;
+	volatile int b = 1;
 	a = b+1;
+    printf("a=%d\n", a);
 #endif
 	// change puts() to __printf_chk() by -O2 optimization 
 	printf("Hello World! gcc --save-temps\n");
