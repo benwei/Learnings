4eaecea9c53334f47ce1468294786d6e4c8a7aa6
Ben Wei <ben@staros.mobi>
2014-04-15 09:58:46 +0800

    [bash] samples name-vale pair to a structure using

---
+++ bashSamples/name_vale_to_a_structure_using.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# sample from http://stackoverflow.com/questions/6046176/bash-adding-a-name-value-to-a-structure-using-bash
+
+declare -A a=(
+        [LC1]="Test1 Test2"
+        [LC2]="Test3 Test4"
+        [LC3]="Test5 Test6"
+        ) 
+
+for k in "${!a[@]}"; do
+    printf '%s\n' "$k"
+    set -- ${a["$k"]} # by default split on white space, tab and newline
+    # you can use another delimiter, if you wish
+    for e; do
+        printf '\t => %s\n' "$e"
+    done
+done 
