a9f261decca0e083cbe34c77fd3911eec7452ae2
ben wei <ben@staros.mobi>
2014-04-15 18:41:01 +0800

    [c] socase header with bool value definitions

--- cSamples/socase/socase.h
+++ cSamples/socase/socase.h
@@ -1,3 +1,7 @@
+/*
+ * Written By Ben Wei ben@staros.mobi 2014
+ **/
+
 #ifndef __SOCASE_H__
 #define __SOCASE_H__
 
@@ -9,6 +13,11 @@ extern "C" {
 #define SO_ASCII_ART_FAIL 'X'
 
 #define SO_CASE_EQUAL(l,r) ((l==r) ? SO_ASCII_ART_OK: SO_ASCII_ART_FAIL)
+
+/* macro and defintions for bool value */
+#define SO_STRING_TRUE "true"
+#define SO_STRING_FALSE "false"
+#define SO_ASCII_BOOL(bool_value) ((bool_value) ? SO_STRING_TRUE: SO_STRING_FALSE) 
 
 #ifdef __cplusplus__
 extern "C" {
