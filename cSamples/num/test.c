4c301338eb248aa91e2424724865e7cd2f31807f
Ben Wei <ben@staros.mobi>
2014-04-24 16:44:57 +0800

    [c] add c90

--- cSamples/num/test.c
+++ cSamples/num/test.c
@@ -1,57 +1,8 @@
-#include <stdio.h>
-#include "test.h"
+#ifndef __TEST_H__
+#define __TEST_H__
 
-int convert_num(ulong val, int len, int right, char *buf)
-{
-	ulong j = 0, k = 0;
-	int i = 0, flag=0;
+typedef unsigned long ulong;
 
-	if (val > 999999999 || len > 9) {
-		return -1;
-	}
-	for(i=0, j=1; i<len-1; i++) {
-		j *= 10;
-	}
-	if (!right) {
-		for (i=0; j>0; j/=10) {
-			k = val/j;
-			if (k > 9) {
-				j *= 100;
-				continue;
-			}
-			if (flag || k || j == 1) {
-				buf[i++] = k + '0';
-				flag++;
-			} else {
-				buf[i++] = ' ';
-			}
-			val -= k * j;
-		}
-	} else {
-		for(i=0; i<len; j/=10) {
-			if (j) {
-				k = val/j;
-					if (k > 9) {
-					j *= 100;
-					len++;
-					continue;
-				}
-				if (k == 0 && flag == 0) {
-					continue;
-				}
-				buf[i++] = k + '0';
-				val -= k * j;
-			} else {
-				if (flag == 0 &&  i < len-1) {
-					buf[i++] = '0';
-				} else {
-					buf[i++] = ' ';
-				}
-			}
-			flag++;
-		}
-	}
+int convert_num(ulong val, int len, int right, char *buf);
 
-	buf[i] = 0;
-    return i;
-}
+#endif /* __TEST_H__ */
