e9d1a2327b0605fe9ae3cae1afc734b7e248353c
Ben6 <ben@staros.mobi>
2014-05-02 20:03:20 +0800

    [c] update mu_check case for minunit

--- cSamples/test_minunit/test1.c
+++ cSamples/test_minunit/test1.c
@@ -1,7 +1,10 @@
 #include "minunit.h"
 
 MU_TEST(test_check) {
-    mu_check(5 == 7);
+    int a = 1, b=2;
+
+    mu_check(a != b);
+    mu_check(b == a + 1);
 }
 MU_TEST_SUITE(test_suite) {
     MU_RUN_TEST(test_check);
