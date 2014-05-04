2b6907c8b8acd10777039f2cd1f4f2824e27c9c1
Ben Wei <ben@staros.mobi>
2014-05-05 00:49:06 +0800

    [c] find minimum value from array with callback comparsion function

--- cSamples/find-min/main.c
+++ cSamples/find-min/main.c
@@ -1,13 +1,20 @@
 #include <stdio.h> /* printf */
 
-int find_min(int *array, int max)
+typedef int (*pfcompare_t)(int, int);
+
+static int compare_min(int left, int right)
+{
+    return left < right;
+}
+
+static int find(int *array, int max, pfcompare_t compare)
 {
     int *array_end = (int *)(array + max);
     int i = 1, min = 0;
 
     for (min = *array, ++array; array < array_end; ++array, ++i) {
         printf("compare min(%2d) and a%d[%2d]\n", min, i, *array);
-        if (*array < min) {
+        if (compare(*array, min) == 1) {
             min = *array;
         }
     }
@@ -18,7 +25,7 @@ int main(int argc, char **argv)
 {
     int input_array[] = {6, 9, 1, 3, 8, 7, 6, 4, 2, 0, 10};
 
-    int min = find_min(input_array, sizeof(input_array)/sizeof(input_array[0])); 
+    int min = find(input_array, sizeof(input_array)/sizeof(input_array[0]), &compare_min); 
     printf("the minmum value is %d\n", min);
     return 0;
 }
