9aa46b175045d064effa1eef7e578010a9ce9ebb
ben wei <ben@staros.mobi>
2014-05-17 17:50:58 +0800

    [nodejs] update join array to string

--- nodejsSamples/testArrayJoin.js
+++ nodejsSamples/testArrayJoin.js
@@ -1,4 +1,4 @@
-
-
-var a = '('+ ['1', '3', '5'].join(', ') + ')'
+#!/usr/bin/env node
+var array = ['1', '3', '5']
+var a = JSON.stringify(array) + '.join(\', \') => '+ array.join(', ');
 console.log(a);
