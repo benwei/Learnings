c255032224c27537b7d3a3fa3d2b6856a1908289
ben wei <ben@staros.mobi>
2014-05-18 14:04:46 +0800

    [py] update more case in binarystring

--- pySamples/test02_binarystring.py
+++ pySamples/test02_binarystring.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
 
-print int('10101010', 2)
+# convert small binary to small decimal
+bin1='10101010'
+print int(bin1, 2)
+#result '170'
 
+# convert big binary string to big decimal
+binbig ='1111111111111111111111111111111111111111111111111111111111111111'
+print int(binbig, 2);
+#result '18446744073709551615'
+
+# convert big decimal to binary
+intbig=18446744073709551615
+print bin(intbig)
+#result '0b1111111111111111111111111111111111111111111111111111111111111111'
+
+# trim the prefix 0b
+print str(bin(intbig))[2:]
+#result '1111111111111111111111111111111111111111111111111111111111111111'
