445ff17bbacf9dcd400945d15bb8652d5d39ca8a
ben wei <ben@staros.mobi>
2014-05-20 10:48:02 +0800

    [c] add base sample for new sample creation

---
+++ cSamples/0_sample_base/main.c
@@ -0,0 +1,11 @@
+/* base sample c project
+ * written by ben6 2014-05
+ */
+
+#include <stdio.h> /* printf */
+
+int main(int argc, char **argv)
+{
+    printf("This is a dummy sample\n");
+    return 0;
+}
