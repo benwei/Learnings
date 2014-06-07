29826619454f56b203596a471ee58fad837c64ad
Ben Wei <ben@staros.mobi>
2014-06-07 11:07:37 +0800

    [c] dump stack and allocated variables with error handling

--- cSamples/callstack/main.c
+++ cSamples/callstack/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 struct callstack {
     int first;
@@ -9,15 +10,20 @@ struct callstack {
 int main(int argc, char **argv)
 {
     struct callstack cs = {20, 10};
-    struct callstack *csm = (struct callstack*) malloc(sizeof(struct callstack));
     printf("callstack address: argc:%p, argv:%p,"
         "cs:%p, cs.first:%p, cs:second:%p\n",
         &argc, argv,
         &cs, &cs.first, &cs.second);
+    {
+    struct callstack *cs_alloc = (struct callstack*) malloc(sizeof(struct callstack));
+    if (cs_alloc == NULL) {
+        return ENOMEM;
+    }
+
     printf("callstack malloc address:"
         "cs:%p, cs.first:%p, cs:second:%p\n",
-        &csm, &csm->first, &csm->second);
-
+        &cs_alloc, &cs_alloc->first, &cs_alloc->second);
+    }
     return 0;
 }
 
