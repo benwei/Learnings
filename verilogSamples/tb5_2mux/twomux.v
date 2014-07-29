921bd83881472cb84cdcebe26eb8a3c12243d92d
ben wei <ben@staros.mobi>
2014-07-30 02:48:32 +0800

    [verilog] sample for using -Dmacro

--- verilogSamples/tb5_2mux/twomux.v
+++ verilogSamples/tb5_2mux/twomux.v
@@ -1,17 +1,25 @@
-`define WIRE_PART 1
+/*
+* iverilog with -Dmacro defines macro with string "1"
+* added to Makefile, you can use the follow command to switch register style
+* ```
+*   make USE_REG=1
+* ```
+**/
 
 module twomux(dina, dinb, sel, dout);
+
 input [3:0] dina,dinb;
 input sel;
 output [3:0] dout;
 
-
 `ifdef WIRE_PART
+/* using wire and assign */
 wire [3:0] dout;
 assign dout[3:0] = (sel == 1'b1) ? dina[3:0] :
                    (sel == 1'b0) ? dinb[3:0] :
                    dout[3:0];
 `else
+/* implement by register, always and case */
 reg [3:0] dout;
 always @(*)
 begin 
