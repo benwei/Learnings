05715d04bae00026abf2c52718d4eb32cdc02a3d
Ben Wei <ben@staros.mobi>
2014-09-09 01:45:54 +0800

    [c] sample for testing new option --as-needed

---
+++ cSamples/test_as_needed_option/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <errno.h>
+#include <linux/unistd.h> 
+#include <linux/kernel.h>
+#include <sys/sysinfo.h>
+
+
+int main()
+{
+    printf("Hello gcc with --as-neededd\n");
+
+    return 0;
+}
