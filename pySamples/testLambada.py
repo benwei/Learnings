d55b53b7addb65fe3509143280912e75fab258b6
ben wei <ben@staros.mobi>
2015-06-25 15:31:48 +0800

    [py] support python3 syntax

--- pySamples/testLambada.py
+++ pySamples/testLambada.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
+import json
 # -*- coding: utf-8 -*-
+# support python 3 syntax
 
 c = lambda x: x**2
 #
 # def c(x):
 #     return x**2
 
-print c(10);
+print(c(10))
 
 m = filter(lambda x: x % 3 == 0, [1,2,20,18,3,6])
-print m
+for i in m:
+    print(i)
 
