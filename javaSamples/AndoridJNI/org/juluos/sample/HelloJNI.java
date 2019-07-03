289b59d8036954fe644da0df9f84602a92776548
Ben Wei <ben@staros.mobi>
2019-07-03 11:26:04 +0800

    [fix/java] revise code to build on osx with android studio

--- javaSamples/AndoridJNI/org/juluos/sample/HelloJNI.java
+++ javaSamples/AndoridJNI/org/juluos/sample/HelloJNI.java
@@ -2,7 +2,7 @@ package org.juluos.sample;
 
 public class HelloJNI {
  static {
-     //System.load("./libhellolib.so");
+     //System.load("/Users/ben_wei/src/github/Learnings/javaSamples/AndoridJNI/libhellolib.so");
      System.loadLibrary("hellolib");
  }
  public native int add(int x ,int y);
